33a2af79be33e4e2bc5a307894cb3fe5b0e01952
X Norman <xnorman@fb.com>
2018-07-10 14:19:11 -0700

    Login Capabilities

--- app/src/main/java/com/plusultra/parsetagram/HomeActivity.java
+++ app/src/main/java/com/plusultra/parsetagram/HomeActivity.java
@@ -2,7 +2,13 @@ package com.plusultra.parsetagram;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.plusultra.parsetagram.model.Post;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,7 +16,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        final Post.Query postQuery = new Post.Query();
+        postQuery
+                .getTop()
+                .withUser();
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null){
+                    for (int i = 0; i < objects.size(); i++){
+                        Log.d("HomeActivity", "Post id: [" + i + "] = " + objects.get(i).getKeyDescription() + "\nusername = " + objects.get(i).getUser().getUsername());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/plusultra/parsetagram/InstaApplication.java
+++ app/src/main/java/com/plusultra/parsetagram/InstaApplication.java
@@ -3,6 +3,8 @@ package com.plusultra.parsetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+import com.plusultra.parsetagram.model.Post;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -11,20 +13,16 @@ public class InstaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // Use for troubleshooting -- remove this line for production
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
-        // Use for monitoring Parse OkHttp traffic
-        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
-        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         builder.networkInterceptors().add(httpLoggingInterceptor);
 
-        // set applicationId, and server server based on the values in the Heroku settings.
-        // clientKey is not needed unless explicitly configured
-        // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("Xstagram") // should correspond to APP_ID env variable
                 .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
