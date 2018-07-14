66eff14c694710c6b0b74fb8838a4c820dac8038
X Norman <xnorman@fb.com>
2018-07-13 17:06:58 -0700

    Final Commit

--- app/src/main/java/com/plusultra/parsetagram/DetailActivity.java
+++ app/src/main/java/com/plusultra/parsetagram/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.plusultra.parsetagram;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -10,11 +9,12 @@ import com.bumptech.glide.Glide;
 import com.parse.ParseUser;
 import com.plusultra.parsetagram.model.Post;
 
+import org.parceler.Parcels;
+
 public class DetailActivity extends AppCompatActivity {
     private ImageView picture;
     private TextView name, caption, time;
-    Post post = new Post();
-    Context context;
+    Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +22,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ParseUser user = ParseUser.getCurrentUser();
+        post = Parcels.unwrap(getIntent().getParcelableExtra("post"));
 
         picture = findViewById(R.id.ivPhoto);
         name = findViewById(R.id.tvName);
         caption = findViewById(R.id.tvCap);
         time = findViewById(R.id.tvTime);
 
-        Glide.with(context)
+        Glide.with(this)
                 .load(post.getImage().getUrl())
                 .into(picture);
 
--- app/src/main/java/com/plusultra/parsetagram/InstaAdapter.java
+++ app/src/main/java/com/plusultra/parsetagram/InstaAdapter.java
@@ -1,6 +1,7 @@
 package com.plusultra.parsetagram;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,25 +13,27 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.plusultra.parsetagram.model.Post;
 
+import org.parceler.Parcels;
+
 import java.util.List;
 
 public class InstaAdapter extends RecyclerView.Adapter<InstaAdapter.ViewHolder>{
     Context context;
-    private List mPosts;
+    List<Post> posts;
 
     public InstaAdapter(List<Post> posts) {
-        mPosts = posts;
+        this.posts = posts;
     }
 
     // Clean all elements of the recycler
     public void clear() {
-        mPosts.clear();
+        posts.clear();
         notifyDataSetChanged();
     }
 
     // Add a list of items -- change to type used
     public void addAll(List<Post> list) {
-        mPosts.addAll(list);
+        posts.addAll(list);
         notifyDataSetChanged();
     }
 
@@ -48,11 +51,12 @@ public class InstaAdapter extends RecyclerView.Adapter<InstaAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         //get data according to position
-        Post post = (Post) mPosts.get(position);
+        Post post = (Post) posts.get(position);
 
         //populate the views according to this data
         holder.tvUser.setText(post.getUser().getUsername());
         holder.tvCaption.setText(post.getDescription());
+        holder.tvStamp.setText(post.getCreatedAt().toString());
 
         Glide.with(context)
                 .load(post.getImage().getUrl())
@@ -61,23 +65,42 @@ public class InstaAdapter extends RecyclerView.Adapter<InstaAdapter.ViewHolder>{
 
     @Override
     public int getItemCount() {
-        return mPosts.size();
+        return posts.size();
     }
 
     //create ViewHolder
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public ImageView ivPicture;
-        public TextView tvUser, tvCaption;
+        public TextView tvUser, tvCaption, tvStamp;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             //findByViewId lookups
             ivPicture = itemView.findViewById(R.id.ivPic);
             tvUser = itemView.findViewById(R.id.etUser);
             tvCaption = itemView.findViewById(R.id.tvCaption);
+            tvStamp = itemView.findViewById(R.id.tvStamp);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //gets item position
+            int position = getAdapterPosition();
+            //make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                //get the movie at the position, this won't work if the class is static
+                Post post = posts.get(position);
+                //create intent for the new activity
+                Intent intent = new Intent(context, DetailActivity.class);
+                // serialize the movie for the new activity
+                intent.putExtra("post", Parcels.wrap(post));
+                //show the activity
+                context.startActivity(intent);
+            }
         }
     }
 }
